43988fb4b8b98398aed0dd63e10fb3137994ef28
User2882 <amssrb3@gmail.com>
2026-05-17 15:32:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello from hausaufgabepy!")
-
+# Hinweis : main.py file habe ich jedes mal aktualisiert, letzte Version davon steht im Ordner HA_Tag7 damit es ordentlich ist ...
 
 if __name__ == "__main__":
     main()
